0d31995e273b5c66056434ba80ae63f89e4abfa6
SKTT1Ryze <linuxgnulover@gmail.com>
2021-06-25 18:10:05 +0800

    cargo fmt

--- src/const_generics/mod.rs
+++ src/const_generics/mod.rs
@@ -1,31 +1,31 @@
 //! Bloom Filter Implementation with Const Generics
-//! 
+//!
 //! In some cases of using bloom filter, the memory size of bloom filter can be determined
 //! in `compile time`. So it's an efficient way to implement bloom filter data structure with `const generics`,
 //! which is stable in rust 1.51 version.
-//! 
+//!
 //! Compared to implementation using `Vec<T>`, there are some advantages:  
 //! + The metadata is placed on the `stack` instead of `heap`, it will reduce some cost of `runtime`
 //! + More elegant way to manage memory
-//! 
+//!
 //! However, there's also some disadvantages:
 //! + Due to floating point arithmetic has not allowed in const fn yet,
 //! we should compute the num of bucket by hand
 //! + Less functionality
-//! 
+//!
 //! Even so, it makes sence to implemet bloom filter with const generics.
 //!
 //! stable bloom filter example:
 //! `cargo.toml`:  
 //! bloom-filters = { git = "https://github.com/nervosnetwork/bloom-filters", features = ["const_generics"]}
 //! rand = "0.6"
-//! 
+//!
 //! ```
 //! use std::collections::hash_map::RandomState;
 //! use rand::{random, thread_rng, Rng};
 //! use rand::distributions::Standard;
 //! use bloom_filters::{BloomFilter, ConstStableBloomFilter, DefaultBuildHashKernels, compute_word_num, stablefilter};
-//! 
+//!
 //! // item count: 10
 //! // bucket size: 3
 //! // fp rate: 0.03
@@ -38,7 +38,7 @@
 //! let items: Vec<usize> = thread_rng().sample_iter(&Standard).take(7).collect();
 //! let _ret: Vec<bool> = items.iter().map(|i| filter.contains(i)).collect();    
 //! ```
-//! 
+//!
 //! classic bloom filter example:
 //! ```
 //! use std::collections::hash_map::RandomState;
@@ -46,7 +46,7 @@
 //! use rand::distributions::Standard;
 //! use bloom_filters::{BloomFilter, ConstClassicBloomFilter, DefaultBuildHashKernels};
 //! use bloom_filters::{compute_word_num, approximate_bucket_count, classicfilter};
-//! 
+//!
 //! // item count: 100
 //! // fp rate: 0.03
 //! let mut filter = classicfilter!(
